78aabd7758819d101dd0671176f7a2942b7b446d
Albert I <kras@raphielgang.org>
2019-08-02 02:09:11 +0800

    grus: Use vendorsetup.sh to apply libvintf patches
    Signed-off-by: Albert I <kras@raphielgang.org>

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+# apply libvintf patches
+workdir=$(pwd)
+cd system/libvintf || return
+git reset -q --hard
+[ "$target" == "grus" ] && patch --merge -sNp1 -i "$workdir"/device/xiaomi/grus/patches/system-libvintf-disable-vbmeta-check.patch
+cd "$workdir" || return

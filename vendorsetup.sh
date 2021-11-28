1c4059bb65d00d70bd8a492d21df2a4700cd2a1a
Albert I <kras@raphielgang.org>
2021-11-28 23:33:57 +0800

    grus: Remove libvintf patches
    * vendorsetup.sh is disallowed from 10+
    
    This reverts commit 78aabd7758819d101dd0671176f7a2942b7b446d.
    This reverts commit 9fa76863a1d345043b7d771747551d6f0aa7084f.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- vendorsetup.sh
+++
@@ -1,6 +0,0 @@
-# apply libvintf patches
-workdir=$(pwd)
-cd system/libvintf || return
-git reset -q --hard
-[ "$target" == "grus" ] && patch --merge -sNp1 -i "$workdir"/device/xiaomi/grus/patches/system-libvintf-disable-vbmeta-check.patch
-cd "$workdir" || return

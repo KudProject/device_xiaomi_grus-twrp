3076eea7828bc9702adf034e9568d5797960e02d
Albert I <kras@raphielgang.org>
2021-11-29 14:10:30 +0800

    grus: Switch to `qcom_decrypt_fbe` for data decryption
    * qcom_decrypt_fbe implies qcom_decrypt being built, so no point defining both
    * BOARD_USES_QCOM_FBE_DECRYPTION implies TW_INCLUDE_CRYPTO_FBE, so no point in
      setting the latter to true either
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- twrp_grus.mk
+++ twrp_grus.mk
@@ -26,6 +26,10 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 PRODUCT_COPY_FILES += \
     $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
 
+# Crypto
+PRODUCT_PACKAGES += \
+    qcom_decrypt_fbe
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys-intf/display

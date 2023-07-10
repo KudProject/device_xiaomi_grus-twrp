b22bd62ef8c0bbde0a67654094db28dd187efc11
SakuraNotStupid <sakurakyuo@protonmail.com>
2023-07-10 14:54:23 +0800

    Cleanup BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,5 +157,3 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
-
--include $(DEVICE_PATH)/BoardConfigSHRP.mk
